fda07bc7e13359ef0f4fb7082a1875b958143794
Sebastian <jchingapalacios05@gmail.com>
2024-10-12 22:34:27 -0500

    Sistema inventario 1.1.2

--- includes/database.php
+++ includes/database.php
@@ -2,7 +2,6 @@
 
 function conectarDB(): mysqli {
     $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
-    // $db = new mysqli('localhost', 'root', 'root', 'inventario');
 
     if (!$db) {
         echo 'Error en la conección';
